7a7f8a18e48eea79bcafe7a6241e25229ee46313
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-09-13 18:08:04 +0200

    feat: add array type

--- app.ts
+++ app.ts
@@ -4,6 +4,16 @@
 // }
 const person = {
     name: 'Maximilian',
-    age: 30
+    age: 30,
+    hobbies: ['Sports', 'Cooking']
+}
+
+// let favoriteActivities: (string | number)[];
+// favoriteActivities = ['sports', 20];
+let favoriteActivities: string[];
+favoriteActivities = ['sports'];
+
+for(const hobby of person.hobbies){
+    console.log('hobby' , hobby);
 }
 console.log('person' , person.name);
