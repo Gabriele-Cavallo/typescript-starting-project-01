c0abaa52823be5779fa72f2dcaa62970f78c1595
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-09-16 16:17:54 +0200

    feat: union types + literal types

--- app.ts
+++ app.ts
@@ -1,19 +1,16 @@
-// const person: {name: string, age:number} = {
-//     name: 'Maximilian',
-//     age: 30
-// }
-const person = {
-    name: 'Maximilian',
-    age: 30,
-    hobbies: ['Sports', 'Cooking']
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text';
+
+function combine(input1: Combinable,input2: Combinable, resultConversion: ConversionDescriptor){
+    let result;
+    if(typeof input1 === 'number'  && typeof input2 === 'number' || resultConversion === 'as-number'){
+        result = +input1 + +input2
+    } else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
 
-// let favoriteActivities: (string | number)[];
-// favoriteActivities = ['sports', 20];
-let favoriteActivities: string[];
-favoriteActivities = ['sports'];
-
-for(const hobby of person.hobbies){
-    console.log('hobby' , hobby);
-}
-console.log('person' , person.name);
+const combinedAges = combine(30, 26, 'as-number');
+const combinedStringAges = combine('30', '26', 'as-number');
+const combinedNames = combine('Max', 'Anna', 'as-text');
